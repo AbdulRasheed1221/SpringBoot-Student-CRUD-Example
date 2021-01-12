9ee8843013c45b4f4f10f5502b9c8522d3b0c01b
rasheed <arshaik1221@gmail.com>
2021-01-12 14:13:07 +0530

    first commit

---
+++ student-Information/src/main/java/com/wipro/Application/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.wipro.Application.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wipro.Application.model.Student;
+
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer>{
+
+}
---
+++ student-Information/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO 
+	STUDENT (first_name, last_name, email,dept) 
+VALUES
+  	('Lokesh', 'Gupta', 'howtodoinjava@gmail.com','FS'),
+  	('John', 'Doe', 'xyz@email.com','FS');
